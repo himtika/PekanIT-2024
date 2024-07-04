10de6185371687987fc7568d75987480f711b67c
anthrofax <afridhoikhsan@gmail.com>
2024-07-04 23:50:49 +0700

    reset

---
+++ src/component/Footer/FooterComponent.tsx
@@ -0,0 +1,60 @@
+export function FooterComponent() {
+  return (
+    <footer className="bg-[#30908e]/30 rounded-lg shadow text-white backdrop-blur-lg	mt-5">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <a
+            href="https://himtika.cs.unsika.ac.id/"
+            className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+          >
+            {/* <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo" 
+            />*/}
+            <span className="self-center text-2xl font-semibold whitespace-nowrap ">
+              HIMTIKA
+            </span>
+          </a>
+          <ul className="flex flex-wrap items-center mb-6 text-sm font-medium  sm:mb-0 ">
+            <li>
+              <a
+                href="https://himtika.cs.unsika.ac.id/"
+                className="hover:underline me-4 md:me-6"
+              >
+                Homepage
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://himtika.cs.unsika.ac.id#kontak-kami"
+                className="hover:underline me-4 md:me-6"
+              >
+                Contact
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://wa.link/xkpkjd"
+                className="hover:underline me-4 md:me-6"
+              >
+                Whatsapp
+              </a>
+            </li>
+          </ul>
+        </div>
+        <hr className="my-6 border-white sm:mx-auto  lg:my-8" />
+        <span className="block text-sm  sm:text-center ">
+          © 2023{" "}
+          <a
+            href="https://himtika.cs.unsika.ac.id/"
+            className="hover:underline"
+          >
+            Himtika™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
---
+++ src/tree/tree.tsx
@@ -0,0 +1,57 @@
+function Tree() {
+  return (
+    <div className="bg-[url('/backgroundImage.jpg')] bg-cover bg-center bg-no-repeat text-white min-h-screen flex flex-col items-center justify-center font-medium">
+      <div className="container text-center mt-5 w-[80%]">
+        <img
+          src="/pekanit-logo.png"
+          className="mb-4 transform transition-transform hover:scale-110 relative left-1/2 -translate-x-1/2"
+          alt="Profile Image"
+          style={{ width: "200px", height: "200px" }}
+        />
+        <h1 className="mb-4 text-3xl">Pekan IT 2024</h1>
+        <p className="mb-5">
+          Pekan IT adalah kegiatan keilmuan yang diadakan oleh Himpunan
+          Mahasiswa Informatika Unsika (HIMTIKA) yang didalamnya terdapat 3
+          agenda yaitu Perlombaan, Seminar dan Workshop Skala Nasional.
+        </p>
+        <div className="list-group flex flex-col items-center">
+          <a
+            href="https://example.com/link1"
+            className="btn btn-custom btn-lg custom-link mt-3 transition-transform transform hover:scale-105 border-4 border-white rounded-full px-4 py-2 text-white bg-transparent hover:bg-white hover:text-black font-bold"
+          >
+            Link Grup Umum Pekan IT
+          </a>
+          <a
+            href="https://example.com/link2"
+            className="btn btn-custom btn-lg custom-link mt-3 transition-transform transform hover:scale-105 border-4 border-white rounded-full px-4 py-2 text-white bg-transparent hover:bg-white hover:text-black font-bold"
+          >
+            Link Guidebook Lomba
+          </a>
+          <a
+            href="https://example.com/link3"
+            className="btn btn-custom btn-lg custom-link mt-3 transition-transform transform hover:scale-105 border-4 border-white rounded-full px-4 py-2 text-white bg-transparent hover:bg-white hover:text-black font-bold"
+          >
+            Link Pendaftaran Perlombaan
+          </a>
+          <a
+            href="https://example.com/link4"
+            className="btn btn-custom btn-lg custom-link mt-3 transition-transform transform hover:scale-105 border-4 border-white rounded-full px-4 py-2 text-white bg-transparent hover:bg-white hover:text-black font-bold"
+          >
+            Link Pendaftaran Seminar
+          </a>
+          <a
+            href="https://example.com/link5"
+            className="btn btn-custom btn-lg custom-link mt-3 transition-transform transform hover:scale-105 border-4 border-white rounded-full px-4 py-2 text-white bg-transparent hover:bg-white hover:text-black font-bold"
+          >
+            Link Pendaftaran Workshop
+          </a>
+        </div>
+      </div>
+      <footer className="text-center mt-4 py-2">
+        <p className="text-sm">&copy; 2024 Pekan IT. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Tree;
