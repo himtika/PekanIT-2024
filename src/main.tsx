1ac9041ca1d2c761d1d27e68e0057d109276f545
anthrofax <afridhoikhsan@gmail.com>
2024-07-05 08:35:12 +0700

    update react router syntax

--- src/main.tsx
+++ src/main.tsx
@@ -3,23 +3,42 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@material-tailwind/react";
 import App from "./App";
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import Dashboard from "./component/Dashboard/Dashboard";
 import Workshop from "./workshop/Workshop";
 import Webinar from "./webinar/Webinar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+  },
+  {
+    path: "/workshop",
+    element: <Workshop />,
+  },
+  {
+    path: "/webinarr",
+    element: <Webinar />,
+  },
+]);
 
 export const rootElement = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/workshop" element={<Workshop />} />
-          <Route path="/webinar" element={<Webinar />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>,
 );
